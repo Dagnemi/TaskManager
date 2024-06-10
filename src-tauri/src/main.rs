9591d7bb889bd33e7da26254a01daedd1d7c76b5
Dag <86215766+Dagnemi@users.noreply.github.com>
2024-06-10 00:43:54 -0300

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,22 +1,8 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn create_task(name: String) -> String {
-    println!("Task {} was created", name);
-    format!("Task was created")
-}
-
-#[tauri::command]
-fn calculate_sum(a: i32, b: i32) {
-    println!("{}", a+b);
-}
-
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![create_task, calculate_sum])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
